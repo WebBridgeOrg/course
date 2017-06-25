d31c8efd11397bff80df35598530cf2a9acee1c2
Mariano Guerra <mariano@marianoguerra.org>
2017-06-25 16:35:51 +0200

    initial import of bootstrap components showcase

---
+++ pages/bootstrap-components/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+BASEDOCS=https://v4-alpha.getbootstrap.com/components/
+
+render () {
+    DOCSLINK=${BASEDOCS}$1/
+    TITLE=$2
+    COMPONENT=$3
+    COMPONENTFILE=$3.part.html
+    OUTFILE=$COMPONENT.html
+    cat page.head.html $COMPONENTFILE page.tail.html > $OUTFILE
+    sed -i "s/%TITLE%/$TITLE/;s#%DOCSLINK%#$DOCSLINK#"  $OUTFILE
+}
+
+render alerts Alerts alerts
+
+
